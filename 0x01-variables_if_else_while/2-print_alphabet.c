db6ff188aeb434b995ea966907b0e0caa9299d30
DorathyUdo <enou99856>
2023-01-16 01:41:45 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,18 @@
-include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - printts the alphabet in lower case
+ * main - printts the alphabet in lower case,
+ * followed by a new line
  * Return: 0 on success
  */
 int main(void)
 {
-	char c = 'a';
-	while (c <= 'z')
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(c);
-		c++
+		putchar(ch);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
